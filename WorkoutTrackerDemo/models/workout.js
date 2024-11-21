eafc507102dd4a3948c6cd35c567f499285ac3e5
Rene27K03 <kageepan.satheeskumar@ontariotechu.net>
2024-11-21 06:39:06 -0500

    Fix

--- WorkoutTrackerDemo/models/workout.js
+++
@@ -1,9 +0,0 @@
-
-                const mongoose = require('mongoose');
-                const workoutSchema = new mongoose.Schema({
-                    name: String,
-                    duration: Number,
-                    type: String
-                });
-                module.exports = mongoose.model('Workout', workoutSchema);
-            
